396c62e9fa19d6eccce2fab7e9c6c69378008c53
redstarht <185753202+redstarht@users.noreply.github.com>
2025-02-04 17:16:54 +0900

    250204

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask,render_template,request,jsonify,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+import pytz
 import sqlite3
 
 
 app = Flask(__name__)
 
-db = 'tests/test.db'
+litedb = 'tests/test.db'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sa_test.db"
+db = SQLAlchemy(app)
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username =  db.Column(db.String(40),nullable=False)
+    password = db.Column(db.String(300),nullable=False)
+    created_at = db.Column(db.DateTime,nullable=False ,default = lambda : datetime.now(pytz.timezone('Asia/Tokyo')))
+
+
+# class User(UserMixin,SQLAlchemydb.Model):
+
 
 def con_db():
-    return sqlite3.connect(db)
+    return sqlite3.connect(litedb)
 
 @app.route("/load_db")
 def load_db():
@@ -52,6 +69,34 @@ def save_recode_to_db(records):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/load_user",methods=['GET','POST'])
+def load_user():
+    if request.method == 'GET':
+        users= user.query.all()
+        return render_template('load_user.html', users=users)
+
+@app.route("/<int:id>/update" , methods=['GET','POST'])
+def update(id):
+    user_id = user.query.get(id)
+    if request.method == 'GET':
+        return render_template("update.html" ,user_id=user_id)
+    else:
+        pass
+
+@app.route("/create",methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        post = user(username=username,password=password)
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('create.html')
 
 @app.route("/save_data",methods=['POST'])
 def save_data():
