70150f32abc30fa4038c150035ec025448113ac6
redstarht <185753202+redstarht@users.noreply.github.com>
2025-02-25 17:27:21 +0900

    250225_company

--- app.py
+++ app.py
@@ -95,18 +95,32 @@ def get_tree():
 @app.route('/api/subsections/<int:section_id>',methods=['GET','POST'])
 def manage_subsection(section_id):
     if request.method == 'GET':
+        print(f"section_id={section_id}")
         # 指定されたsection_idに紐づいた子係(subsections)を取得
         subsections = Subsection.query.filter_by(section_id=section_id).all()
-        return jsonify ([sub.to_dict() for sub in subsections])
+
+        if not subsections:
+            print("データが見つかりませんでした[]を返します")
+            return jsonify([])
+        
+        response = jsonify ([sub.to_dict() for sub in subsections])
+        print(f"レスポンスデータ:{response.get_json}")
+        return response
     
     # 更新・保存機能
     elif request.method =='POST':
         data = request.json
+
+        if not data:
+            print("エラーです、リクエストデータがNone")
+            return jsonify({"error":"リクエストボディが空です"},400)
+
         exsiting_subsections = Subsection.query.filter_by(section_id=section_id).all()
 
         # 既存データを更新 or 削除
         for i , sub in enumerate(exsiting_subsections):
             if i < len(data):
+                # キーエラー防止
                 sub.name = data[i]['name']
             else:
                 # 余分なデータは削除
@@ -118,6 +132,7 @@ def manage_subsection(section_id):
             db.session.add(new_sub)
 
         db.session.commit()
+        print(f"更新後のsubsections:{[sub.to_dict() for sub in Subsection.query.filter_by(section_id=section_id).all()]}")
         return jsonify({"message":"subsections updated successfully"})
 
 
